4c32757835cd64a19c34ee2e0a88c94c540a6bc7
Andrew Grant <angran@amazon.com>
2020-07-24 14:16:22 -0400

    Change delay

--- aws-kendra-index/src/main/java/software/amazon/kendra/index/BaseHandlerStd.java
+++ aws-kendra-index/src/main/java/software/amazon/kendra/index/BaseHandlerStd.java
@@ -15,8 +15,13 @@ import java.time.Duration;
 public abstract class BaseHandlerStd extends BaseHandler<CallbackContext> {
 
   protected static Constant STABILIZATION_DELAY = Constant.of()
-          .timeout(Duration.ofDays(100L))
-          .delay(Duration.ofSeconds(20L))
+          // Set the timeout to something silly/way too high, because
+          // we already set the timeout in the schema https://github.com/aws-cloudformation/aws-cloudformation-resource-schema
+          .timeout(Duration.ofDays(365L))
+          // Set the delay to two minutes so the stabilization code only calls
+          // DescribeIndex every two minutes - stabilization (create, update, delete) takes
+          // 30/45+ minutes so there's no need to check the index is active more than every couple minutes.
+          .delay(Duration.ofMinutes(2))
           .build();
 
   @Override
