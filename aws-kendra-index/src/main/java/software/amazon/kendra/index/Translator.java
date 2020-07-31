76116ff8ce1a5ed8c6bf171874ba5a55ddca49d2
Andrew Grant <angran@amazon.com>
2020-07-31 11:18:11 -0400

    Another rename

--- aws-kendra-index/src/main/java/software/amazon/kendra/index/Translator.java
+++ aws-kendra-index/src/main/java/software/amazon/kendra/index/Translator.java
@@ -230,7 +230,7 @@ public class Translator {
 
   static List<DocumentMetadataConfiguration> translateToSdkDocumentMetadataConfigurationList(
           List<software.amazon.kendra.index.DocumentMetadataConfiguration> attributesDefinedInCFTemplate,
-          Map<String, String> attributesDefinedAtIndexLevel) throws TranslatorValidationException {
+          Map<String, String> attributesDefinedOnIndex) throws TranslatorValidationException {
 
     // Document metadata configuration directly defined/requested in the CloudFormation template
     List<DocumentMetadataConfiguration> sdkAttributesDefinedInCFTemplate =
@@ -239,7 +239,7 @@ public class Translator {
             .stream().collect(Collectors.toMap(x -> x.name(), x -> x.typeAsString()));
 
     List<DocumentMetadataConfiguration> sdkDefaultAttributes = new ArrayList<>();
-    for (Map.Entry<String, String> entry : attributesDefinedAtIndexLevel.entrySet()) {
+    for (Map.Entry<String, String> entry : attributesDefinedOnIndex.entrySet()) {
       // If the attribute is a reserved one (i.e. it is prefixed with "_") ...
       if (entry.getKey().startsWith("_")) {
         // and it's not in the requested CloudFormation template,
