dbc90fb5a82fd3e37b449b619162ab5073253d05
waquarahmad515 <68386127+waquarahmad515@users.noreply.github.com>
2020-07-31 11:54:22 -0400

    Adding converter for sharepoint (#34)

---
+++ aws-kendra-data-source/src/main/java/software/amazon/kendra/datasource/convert/SharePointConverter.java
@@ -0,0 +1,112 @@
+package software.amazon.kendra.datasource.convert;
+
+import software.amazon.kendra.datasource.DataSourceConfiguration;
+import software.amazon.kendra.datasource.DataSourceToIndexFieldMapping;
+import software.amazon.kendra.datasource.DataSourceVpcConfiguration;
+import software.amazon.kendra.datasource.SharePointConfiguration;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class SharePointConverter {
+
+  public static software.amazon.awssdk.services.kendra.model.DataSourceConfiguration sdkDataSourceConfiguration(
+    SharePointConfiguration sharePointConfiguration) {
+    return software.amazon.awssdk.services.kendra.model.DataSourceConfiguration.builder()
+    .sharePointConfiguration(software.amazon.awssdk.services.kendra.model.SharePointConfiguration.builder()
+      .sharePointVersion(sharePointConfiguration.getSharePointVersion())
+      .urls(sharePointConfiguration.getUrls())
+      .secretArn(sharePointConfiguration.getSecretArn())
+      .crawlAttachments(sharePointConfiguration.getCrawlAttachments())
+      .useChangeLog(sharePointConfiguration.getUseChangeLog())
+      .inclusionPatterns(sharePointConfiguration.getInclusionPatterns())
+      .exclusionPatterns(sharePointConfiguration.getExclusionPatterns())
+      .vpcConfiguration(sdkVpcConfiguration(sharePointConfiguration.getVpcConfiguration()))
+      .fieldMappings(sdkFieldMappings(sharePointConfiguration.getFieldMappings()))
+      .documentTitleFieldName(sharePointConfiguration.getDocumentTitleFieldName())
+      .build())
+    .build();
+  }
+
+  public static DataSourceConfiguration modelDataSourceConfiguration(
+    software.amazon.awssdk.services.kendra.model.SharePointConfiguration sharePointConfiguration) {
+    return DataSourceConfiguration.builder()
+      .sharePointConfiguration(SharePointConfiguration.builder()
+      .sharePointVersion(sharePointConfiguration.sharePointVersion().toString())
+      .urls(sharePointConfiguration.urls())
+      .secretArn(sharePointConfiguration.secretArn())
+      .crawlAttachments(sharePointConfiguration.crawlAttachments())
+      .useChangeLog(sharePointConfiguration.useChangeLog())
+      .inclusionPatterns(sharePointConfiguration.inclusionPatterns())
+      .exclusionPatterns(sharePointConfiguration.exclusionPatterns())
+      .vpcConfiguration(modelVpcConfiguration(sharePointConfiguration.vpcConfiguration()))
+      .fieldMappings(modelFieldMappings(sharePointConfiguration.fieldMappings()))
+      .documentTitleFieldName(sharePointConfiguration.documentTitleFieldName())
+      .build())
+    .build();
+  }
+
+  static software.amazon.awssdk.services.kendra.model.DataSourceVpcConfiguration sdkVpcConfiguration(
+    DataSourceVpcConfiguration dataSourceVpcConfiguration) {
+    if (dataSourceVpcConfiguration == null) {
+      return null;
+    }
+    return software.amazon.awssdk.services.kendra.model.DataSourceVpcConfiguration.builder()
+      .securityGroupIds(dataSourceVpcConfiguration.getSecurityGroupIds())
+      .subnetIds(dataSourceVpcConfiguration.getSubnetIds())
+      .build();
+  }
+
+  static List<software.amazon.awssdk.services.kendra.model.DataSourceToIndexFieldMapping> sdkFieldMappings(
+    List<DataSourceToIndexFieldMapping> fieldMappings) {
+    if (fieldMappings == null) {
+     return null;
+    }
+    return fieldMappings.stream()
+     .filter(Objects::nonNull)
+     .map(fieldMapping -> sdkFieldMapping(fieldMapping))
+     .collect(Collectors.toList());
+  }
+
+  static software.amazon.awssdk.services.kendra.model.DataSourceToIndexFieldMapping sdkFieldMapping(
+    DataSourceToIndexFieldMapping fieldMapping) {
+    return software.amazon.awssdk.services.kendra.model.DataSourceToIndexFieldMapping.builder()
+      .dataSourceFieldName(fieldMapping.getDataSourceFieldName())
+      .dateFieldFormat(fieldMapping.getDateFieldFormat())
+      .indexFieldName(fieldMapping.getIndexFieldName())
+      .build();
+  }
+
+  static DataSourceVpcConfiguration modelVpcConfiguration(software.amazon.awssdk.services.kendra.model.DataSourceVpcConfiguration
+    dataSourceVpcConfiguration) {
+    if (dataSourceVpcConfiguration == null) {
+      return null;
+    }
+    return DataSourceVpcConfiguration.builder()
+      .securityGroupIds(dataSourceVpcConfiguration.securityGroupIds())
+      .subnetIds(dataSourceVpcConfiguration.subnetIds())
+      .build();
+  }
+
+  static List<DataSourceToIndexFieldMapping> modelFieldMappings(
+    List<software.amazon.awssdk.services.kendra.model.DataSourceToIndexFieldMapping> fieldMappings) {
+    if (fieldMappings == null) {
+     return null;
+    }
+    return fieldMappings.stream()
+     .filter(Objects::nonNull)
+     .map(fieldMapping -> modelFieldMapping(fieldMapping))
+     .collect(Collectors.toList());
+  }
+
+  static DataSourceToIndexFieldMapping modelFieldMapping(
+    software.amazon.awssdk.services.kendra.model.DataSourceToIndexFieldMapping fieldMapping) {
+    return DataSourceToIndexFieldMapping.builder()
+      .dataSourceFieldName(fieldMapping.dataSourceFieldName())
+      .dateFieldFormat(fieldMapping.dateFieldFormat())
+      .indexFieldName(fieldMapping.indexFieldName())
+      .build();
+  }
+
+}
---
+++ aws-kendra-data-source/src/test/java/software/amazon/kendra/datasource/TranslatorTest.java
@@ -0,0 +1,176 @@
+package software.amazon.kendra.datasource;
+
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.kendra.model.CreateDataSourceRequest;
+import software.amazon.awssdk.services.kendra.model.DeleteDataSourceRequest;
+import software.amazon.awssdk.services.kendra.model.DescribeDataSourceRequest;
+import software.amazon.awssdk.services.kendra.model.ListDataSourcesRequest;
+import software.amazon.awssdk.services.kendra.model.TagResourceRequest;
+import software.amazon.awssdk.services.kendra.model.UntagResourceRequest;
+import software.amazon.awssdk.services.kendra.model.UpdateDataSourceRequest;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+
+public class TranslatorTest {
+
+    @Test
+    void testTranslateToReadRequest() {
+        String id = "id";
+        String indexId = "indexId";
+        ResourceModel resourceModel = ResourceModel
+                .builder()
+                .id(id)
+                .indexId(indexId)
+                .build();
+        DescribeDataSourceRequest describeDataSourceRequest = Translator.translateToReadRequest(resourceModel);
+        assertThat(describeDataSourceRequest.id()).isEqualTo(id);
+        assertThat(describeDataSourceRequest.indexId()).isEqualTo(indexId);
+    }
+
+    @Test
+    void testTranslateToDeleteRequest() {
+        String id = "id";
+        String indexId = "indexId";
+        ResourceModel resourceModel = ResourceModel
+                .builder()
+                .id(id)
+                .indexId(indexId)
+                .build();
+        DeleteDataSourceRequest deleteIndexRequest = Translator.translateToDeleteRequest(resourceModel);
+        assertThat(deleteIndexRequest.id()).isEqualTo(id);
+        assertThat(deleteIndexRequest.indexId()).isEqualTo(indexId);
+    }
+
+    @Test
+    void testTranslateToCreateRequest_WithS3DataSource() {
+        String indexId = "indexId";
+        ResourceModel resourceModel = ResourceModel
+            .builder()
+            .indexId(indexId)
+            .type("S3")
+            .dataSourceConfiguration(DataSourceConfiguration.builder()
+                .s3Configuration(S3DataSourceConfiguration.builder().build())
+                .build())
+            .build();
+        CreateDataSourceRequest createDataSourceRequest = Translator.translateToCreateRequest(resourceModel);
+        assertThat(createDataSourceRequest.indexId()).isEqualTo(indexId);
+        assertThat(createDataSourceRequest.configuration().s3Configuration()).isNotNull();
+    }
+
+    @Test
+    void testTranslateToCreateRequest_WithSharePointDataSource() {
+        String indexId = "indexId";
+        ResourceModel resourceModel = ResourceModel
+            .builder()
+            .indexId(indexId)
+            .type("SHAREPOINT")
+            .dataSourceConfiguration(DataSourceConfiguration.builder()
+                .sharePointConfiguration(SharePointConfiguration.builder().build())
+                .build())
+            .build();
+        CreateDataSourceRequest createDataSourceRequest = Translator.translateToCreateRequest(resourceModel);
+        assertThat(createDataSourceRequest.indexId()).isEqualTo(indexId);
+        assertThat(createDataSourceRequest.configuration().sharePointConfiguration()).isNotNull();
+    }
+
+    @Test
+    void testTranslateToCreateRequest_WithInvalidDataSource() {
+        String indexId = "indexId";
+        ResourceModel resourceModel = ResourceModel
+            .builder()
+            .indexId(indexId)
+            .description("description")
+            .build();
+        CreateDataSourceRequest createDataSourceRequest = Translator.translateToCreateRequest(resourceModel);
+        assertThat(createDataSourceRequest.indexId()).isEqualTo(indexId);
+        assertThat(createDataSourceRequest.configuration()).isEqualTo(null);
+    }
+
+    @Test
+    void testTranslateToCreateRequest_WithTags() {
+        String indexId = "indexId";
+        ResourceModel resourceModel = ResourceModel
+            .builder()
+            .indexId(indexId)
+            .tags(Arrays.asList(Tag.builder().key("key").value("value").build()))
+            .build();
+        CreateDataSourceRequest createDataSourceRequest = Translator.translateToCreateRequest(resourceModel);
+        assertThat(createDataSourceRequest.indexId()).isEqualTo(indexId);
+        assertThat(createDataSourceRequest.tags().size()).isEqualTo(1);
+        assertThat(createDataSourceRequest.tags().get(0).key()).isEqualTo("key");
+        assertThat(createDataSourceRequest.tags().get(0).value()).isEqualTo("value");
+    }
+
+    @Test
+    void testTranslateToUpdateRequest() {
+        String id = "id";
+        String indexId = "indexId";
+        ResourceModel resourceModel = ResourceModel
+            .builder()
+            .id(id)
+            .indexId(indexId)
+            .description("description")
+            .build();
+        UpdateDataSourceRequest updateDataSourceRequest = Translator.translateToUpdateRequest(resourceModel);
+        assertThat(updateDataSourceRequest.id()).isEqualTo(id);
+        assertThat(updateDataSourceRequest.indexId()).isEqualTo(indexId);
+        assertThat(updateDataSourceRequest.description()).isEqualTo("description");
+        assertThat(updateDataSourceRequest.name()).isEqualTo("");
+        assertThat(updateDataSourceRequest.roleArn()).isEqualTo("");
+        assertThat(updateDataSourceRequest.schedule()).isEqualTo("");
+        assertThat(updateDataSourceRequest.configuration())
+            .isEqualTo(software.amazon.awssdk.services.kendra.model.DataSourceConfiguration.builder().build());
+    }
+
+    @Test
+    void testTranslateToListRequest()  {
+        String nextToken = "nextToken";
+        String indexId = "indexId";
+        ListDataSourcesRequest listDataSourcesRequest = Translator.translateToListRequest(indexId, nextToken);
+        assertThat(listDataSourcesRequest.indexId()).isEqualTo(indexId);
+        assertThat(listDataSourcesRequest.nextToken()).isEqualTo(nextToken);
+    }
+
+    @Test
+    void testTranslateToUntagResourceRequest() {
+        String arn = "arn";
+        String key = "key";
+        String value = "value";
+        Tag tag = Tag.builder().key(key).value(value).build();
+        HashSet<Tag> tags = new HashSet<>();
+        tags.add(tag);
+        UntagResourceRequest actual = Translator.translateToUntagResourceRequest(tags, arn);
+        UntagResourceRequest expected = UntagResourceRequest
+                .builder()
+                .resourceARN(arn)
+                .tagKeys(Arrays.asList(key))
+                .build();
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void testTranslateToTagResourceRequest() {
+        String arn = "arn";
+        String key = "key";
+        String value = "value";
+        Tag tag = Tag.builder().key(key).value(value).build();
+        HashSet<Tag> tags = new HashSet<>();
+        tags.add(tag);
+        TagResourceRequest actual = Translator.translateToTagResourceRequest(tags, arn);
+        TagResourceRequest expected = TagResourceRequest
+                .builder()
+                .resourceARN(arn)
+                .tags(Arrays.asList(software.amazon.awssdk.services.kendra.model.Tag
+                        .builder()
+                        .key(key)
+                        .value(value)
+                        .build()))
+                .build();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
+}
