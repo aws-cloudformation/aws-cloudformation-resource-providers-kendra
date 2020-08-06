d916fa63269aa5e176acb9661a5f3d7369015056
Andrew Grant <angran@amazon.com>
2020-08-06 10:09:01 -0400

    Fix indenting

--- aws-kendra-data-source/src/main/java/software/amazon/kendra/datasource/DeleteHandler.java
+++ aws-kendra-data-source/src/main/java/software/amazon/kendra/datasource/DeleteHandler.java
@@ -46,33 +46,33 @@ public class DeleteHandler extends BaseHandlerStd {
     }
 
     protected ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final ProxyClient<KendraClient> proxyClient,
-        final Logger logger) {
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final ProxyClient<KendraClient> proxyClient,
+            final Logger logger) {
 
         this.logger = logger;
 
         final ResourceModel model = request.getDesiredResourceState();
 
         return ProgressEvent.progress(model, callbackContext)
-            // STEP 2.0 [delete/stabilize progress chain - required for resource deletion]
-            .then(progress ->
-                // If your service API throws 'ResourceNotFoundException' for delete requests then DeleteHandler can return just proxy.initiate construction
-                // STEP 2.0 [initialize a proxy context]
-                proxy.initiate("AWS-Kendra-DataSource::Delete", proxyClient, model, callbackContext)
+                // STEP 2.0 [delete/stabilize progress chain - required for resource deletion]
+                .then(progress ->
+                        // If your service API throws 'ResourceNotFoundException' for delete requests then DeleteHandler can return just proxy.initiate construction
+                        // STEP 2.0 [initialize a proxy context]
+                        proxy.initiate("AWS-Kendra-DataSource::Delete", proxyClient, model, callbackContext)
 
-                    // STEP 2.1 [construct a body of a request]
-                    .translateToServiceRequest(Translator::translateToDeleteRequest)
-                    .backoffDelay(delay)
-                    // STEP 2.2 [ make an api call]
-                    .makeServiceCall(this::deleteDataSource)
+                                // STEP 2.1 [construct a body of a request]
+                                .translateToServiceRequest(Translator::translateToDeleteRequest)
+                                .backoffDelay(delay)
+                                // STEP 2.2 [ make an api call]
+                                .makeServiceCall(this::deleteDataSource)
 
-                    // STEP 2.3 [stabilize step is not necessarily required but typically involves describing the resource until it is in a certain status, though it can take many forms]
-                    // for more information -> https://docs.aws.amazon.com/cloudformation-cli/latest/userguide/resource-type-test-contract.html
-                        .stabilize(this::stabilizedOnDelete)
-                        .done(this::setResourceModelToNullAndReturnSuccess));
+                                // STEP 2.3 [stabilize step is not necessarily required but typically involves describing the resource until it is in a certain status, though it can take many forms]
+                                // for more information -> https://docs.aws.amazon.com/cloudformation-cli/latest/userguide/resource-type-test-contract.html
+                                .stabilize(this::stabilizedOnDelete)
+                                .done(this::setResourceModelToNullAndReturnSuccess));
     }
 
     private ProgressEvent<ResourceModel, CallbackContext> setResourceModelToNullAndReturnSuccess(
@@ -93,12 +93,12 @@ public class DeleteHandler extends BaseHandlerStd {
      * @return delete resource response
      */
     private DeleteDataSourceResponse deleteDataSource(
-        final DeleteDataSourceRequest deleteDataSourceRequest,
-        final ProxyClient<KendraClient> proxyClient) {
+            final DeleteDataSourceRequest deleteDataSourceRequest,
+            final ProxyClient<KendraClient> proxyClient) {
         DeleteDataSourceResponse deleteDataSourceResponse;
         try {
             deleteDataSourceResponse = proxyClient.injectCredentialsAndInvokeV2(
-                deleteDataSourceRequest, proxyClient.client()::deleteDataSource);
+                    deleteDataSourceRequest, proxyClient.client()::deleteDataSource);
         } catch (final ResourceNotFoundException e) {
             throw new CfnNotFoundException(ResourceModel.TYPE_NAME, deleteDataSourceRequest.id(), e);
         } catch (ConflictException e) {
@@ -128,16 +128,16 @@ public class DeleteHandler extends BaseHandlerStd {
      * @return boolean state of stabilized or not
      */
     private boolean stabilizedOnDelete(
-        final DeleteDataSourceRequest deleteDataSourceRequest,
-        final DeleteDataSourceResponse deleteDataSourceResponse,
-        final ProxyClient<KendraClient> proxyClient,
-        final ResourceModel model,
-        final CallbackContext callbackContext) {
+            final DeleteDataSourceRequest deleteDataSourceRequest,
+            final DeleteDataSourceResponse deleteDataSourceResponse,
+            final ProxyClient<KendraClient> proxyClient,
+            final ResourceModel model,
+            final CallbackContext callbackContext) {
 
         DescribeDataSourceRequest describeDataSourceRequest = DescribeDataSourceRequest.builder()
-            .id(model.getId())
-            .indexId(model.getIndexId())
-            .build();
+                .id(model.getId())
+                .indexId(model.getIndexId())
+                .build();
 
         boolean stabilized;
         try {
@@ -147,7 +147,7 @@ public class DeleteHandler extends BaseHandlerStd {
             stabilized = true;
         }
         logger.log(String.format("%s [%s] deletion has stabilized: %s",
-             ResourceModel.TYPE_NAME, model.getPrimaryIdentifier(), stabilized));
+                ResourceModel.TYPE_NAME, model.getPrimaryIdentifier(), stabilized));
         return stabilized;
     }
 }
